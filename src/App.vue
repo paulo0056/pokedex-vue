ff474a6cd5383259cb322f4a7e9bcd18272dd693
Paulo André <pauloandre.dev@gmail.com>
2021-08-12 18:39:38 -0300

    dialog terminado
    dialog terminado

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ export default Vue.extend(
   data() {
     return {
       apiUrl: 'https://pokeapi.co/api/v2/pokemon/',
-      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/',
+      imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/',
     
      
 
--- src/components/PokemonList.vue
+++ src/components/PokemonList.vue
@@ -40,7 +40,7 @@
           >
             <div class="poke-card" @click="dialogOn(index)">
               <img
-                :src="imageUrl + ((index + 1) + 20 * page) + '.svg'"
+                :src="imageUrl + ((index + 1) + 20 * page) + '.png'"
                 width="70"
                 height="70"
                 class="poke-img"
@@ -75,17 +75,59 @@
       </v-row>
     </div>
 
-    <v-dialog v-model="showDialog" width="500"
+    <v-dialog v-model="showDialog" width="550"
       ><div class="bg">
         <img
-          :src="imageUrl + id + '.svg'"
-          width="70"
-          height="70"
-          class="poke-img"
+          :src="imageUrl + id + '.png'"
+          width="200"
+          height="200"
+          class="poke-dialog"
         />
-        <div>
-          {{detail.name}}
+        <v-row>
+          <v-col cols="12" class="d-flex justify-center">
+            <h2 class="dialog-title red--text">{{detail.name}}</h2>
+          
+        </v-col>
+        <v-col cols="6" class="d-flex justify-center ml-9 nome-poke-sm">
+          <h4 class="dialog-title brown--text">Peso:</h4>
+          <h4 class="dialog-title"> {{detail.weight}}kg</h4>
+         
+        </v-col>
+        <v-col cols="3" class="d-flex justify-center">
+          <h4 class="dialog-title brown--text">Altura:</h4>
+          <h4 class="dialog-title">{{detail.height}}m</h4>
+         
+        </v-col>
+         <v-col cols="6" class="d-flex justify-end">
+          <h4 class="dialog-title">Tipo:</h4>
+         
+        </v-col>
+        
+        <v-col cols="1"  v-for="(type, t) in detail.types"
+           :key="t" class="d-flex justify-center">   
+             <ul><li><h4 class="dialog-title pink--text">{{type}}</h4></li></ul>
+        </v-col>
+        </v-row>
+        
+        <div  >
+          
         </div>
+         <v-row justify="center" align="center" class="ma-2 mt-7">
+           <v-col cols="2" v-for="(item, s) in status"
+           :key="s">
+             <div class="d-flex justify-center font-weight-bold green--text"><div class="h5-new">{{item.nome}}</div></div>
+           </v-col>
+           <v-col cols="2"  v-for="base_stat in detail.stats"
+           :key="base_stat.effort">
+             <div class="d-flex justify-center font-weight-bold indigo--text"><h4>{{base_stat}}</h4></div>
+           </v-col>
+           
+           
+          
+        </v-row>
+       
+     
+    
       
       
 
@@ -100,13 +142,23 @@ export default {
   props: ["apiUrl", "imageUrl"],
   data() {
     return {
+      contador: 0,
       pokemons: [],
       show: false,
       id: 0,
       page: 0,
       showDialog: false,
       detail: [],
-      tipos: [],
+      tipos: {},
+      status:[
+        {nome: 'HP'}, 
+        {nome: 'ATK'}, 
+        {nome: 'DEF'}, 
+        {nome: 'SP.ATK'},
+        {nome: 'SP.DEF'},
+        {nome: 'SPEED'},
+
+      ]
      
     };
   },
@@ -145,25 +197,44 @@ export default {
       this.fetchPokemons(this.pokemons.next);
       this.show = true;
       this.page = this.page + 1;
+      this.contador ++;
     },
     previous() {
       this.fetchPokemons(this.pokemons.previous);
-      this.page = this.page - 1;
+         this.page = this.page - 1;
+         this.contador --;
+      
+      if(this.contador == 0){
+         this.show = false
+      } 
+          
+     
     },
     async dialogOn(index) {
       this.id = index + 1 + 20 * this.page;
-      await axios
+     /*  await axios
         .get(`https://pokeapi.co/api/v2/pokemon/${index + 1 + 20 * this.page}`)
         .then((res) => {
           this.detail = res.data;
-          this.tipos = res.data.map(info => {
+          this.tipos = res.data.map(type => {
           return {
-          typeName: info.name
+          typeName: type.name
           }
           })
           
         })
-        .catch((e) => console.log(e));
+        .catch((e) => console.log(e)); */
+        const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${index + 1 + 20 * this.page}`).then(res => res.json());
+         this.detail = {
+          name: data.name,
+          number: data.id,
+          weight: data.weight,
+          height: data.height,
+          types: data.types.map(({ type }) => type.name),
+          stats: data.stats.map((stats) => stats.base_stat)
+        }
+
+        
          this.showDialog = true
       
             
@@ -180,6 +251,28 @@ export default {
 </script>
 
 <style>
+
+.atks{
+  margin: 10px;
+}
+.dialog-title{
+  margin-right: 40px;
+  text-transform: capitalize;
+}
+.poke-dialog{
+  margin-left: 25%;
+  margin-top: 30px;
+}
+li{
+  display: inline-block;
+}
+.h5-new{
+  font-size: 15px;
+}
+ul{
+  list-style-type: none;
+  display: inline-block;
+}
 .search-box {
   border: solid 1px;
   height: 50px;
@@ -189,6 +282,13 @@ export default {
   .botoes {
     display: none !important;
   }
+  .dialog-title{
+  margin-right: 0;
+}
+.h5-new{
+  font-size: 13px;
+}
+
 }
 @media (min-width: 960px) {
   .botoes-lg {
@@ -209,6 +309,7 @@ export default {
 
 .bg {
   background-color: #fff;
+  height: 550px;
 }
 
 .poke-card {
